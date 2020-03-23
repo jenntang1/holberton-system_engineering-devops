768ea0bcc195b7de40f1093fea87bc9e434cf722
jenntang1 <1039@holbertonschool.com>
2020-03-23 19:48:34 +0000

    2. Updating with provider attribute and correct paths

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 # This manifest creates a manifest that kills a process.
 exec { 'killmenow':
-  command => 'pkill killmenow',
-  path    => '/killmenow/'
+  command  => 'pkill killmenow',
+  path     => ['/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/'],
+  provider => 'shell'
 }
