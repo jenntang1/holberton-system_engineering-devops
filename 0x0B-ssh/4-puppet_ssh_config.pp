7de1652a7b3ac1d02859695c01ba76c054b2c6dc
jenntang1 <1039@holbertonschool.com>
2020-03-25 21:29:38 +0000

    3. Adding wildcards

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -2,12 +2,12 @@
 file_line { 'Turn off passwd auth':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => 'IdentityFile ~/.ssh/holberton',
-  match  => 'IdentityFile *'
+  line   => '*IdentityFile ~/.ssh/holberton',
+  match  => '*IdentityFile *'
 }
 file_line { 'Declare identity file':
   ensure => 'present',
   path   => '/etc/ssh/ssh_config',
-  line   => 'PasswordAuthentication no',
+  line   => '*PasswordAuthentication no',
   match  => '*PasswordAuthentication yes'
 }
