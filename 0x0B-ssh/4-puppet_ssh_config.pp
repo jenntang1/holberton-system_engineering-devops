f1f10229822f4ba87463eec6d9fdd2d2d9760259
jenntang1 <1039@holbertonschool.com>
2020-03-25 21:26:58 +0000

    2. Updating match attribute in Turn off passwd auth

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,11 +1,13 @@
 # This manifest sets up a SSH config file.
 file_line { 'Turn off passwd auth':
-  path  => '/etc/ssh/ssh_config',
-  line  => 'IdentityFile ~/.ssh/holberton',
-  match => 'IdentityFile *'
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentityFile ~/.ssh/holberton',
+  match  => 'IdentityFile *'
 }
 file_line { 'Declare identity file':
-  path  => '/etc/ssh/ssh_config',
-  line  => 'PasswordAuthentication no',
-  match => 'PasswordAuthentication yes'
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+  match  => '*PasswordAuthentication yes'
 }
