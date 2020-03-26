0226684261c4fad1175ca3cb011dc0b2a22fffe5
jenntang1 <1039@holbertonschool.com>
2020-03-26 21:52:58 +0000

    3. Updating shell command

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,5 @@
 # This manifest installs Ngnix.
 exec { 'ngnix':
-  command  => 'sudo apt-get update && sudo apt-get -y install nginx && echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html && sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default-backup && var="\tserver_name jenntang1.tech;\n\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" && sudo sed -i "35i\ $var" /etc/nginx/sites-available/default && sudo service nginx start',
+  command  => 'sudo apt-get update && sudo apt-get -y install nginx && echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html && var="rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" && sudo sed -i "35i$var" /etc/nginx/sites-available/default && sudo service nginx start',
   provider => shell
 }
