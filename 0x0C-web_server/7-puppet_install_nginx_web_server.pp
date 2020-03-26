d70833d330d9c9b7a1f99744dbcf6b591d711ae8
jenntang1 <1039@holbertonschool.com>
2020-03-26 19:53:58 +0000

    2. Changing to exec resouce type

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,2 +1,5 @@
-# This manifest install Ngnix.
-class { 'nginx': }
+# This manifest installs Ngnix.
+exec { 'ngnix':
+  command  => 'sudo apt-get update && sudo apt-get -y install nginx && echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html && sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default-backup && var="\tserver_name jenntang1.tech;\n\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" && sudo sed -i "35i\ $var" /etc/nginx/sites-available/default && sudo service nginx start',
+  provider => shell
+}
