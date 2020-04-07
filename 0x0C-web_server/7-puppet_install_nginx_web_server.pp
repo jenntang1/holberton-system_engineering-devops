ba78dc1f402887d5877d61d325351806b99b229f
jenntang1 <1039@holbertonschool.com>
2020-04-07 23:29:46 +0000

    4. Updating with sudo commands

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,5 @@
 # This manifest installs Ngnix.
 exec { 'ngnix':
-  command  => 'sudo apt-get update && sudo apt-get -y install nginx && echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html && var="rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" && sudo sed -i "35i$var" /etc/nginx/sites-available/default && sudo service nginx start',
+  command  => 'sudo apt-get update && sudo apt-get -y install nginx && sudo echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html && var="rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" && sudo sed -i "35i$var" /etc/nginx/sites-available/default && sudo service nginx start',
   provider => shell
 }
