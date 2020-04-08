c41f3cf6a8500bb2d30d6b55e2ad4420e132113e
jenntang1 <1039@holbertonschool.com>
2020-04-08 03:30:18 +0000

    2. Updating with correct shell commands

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,5 +1,5 @@
 # This manifest adds a custom HTTP header.
 exec { 'http':
-  command  => 'sudo apt-get update && sudo apt-get -y install nginx && var="add_header X-Served-By: $HOSTNAME" && sudo sed -i "50i$var" /etc/nginx/sites-available/default && sudo service nginx start',
+  command  => 'sudo apt-get update && sudo apt-get -y install nginx && sudo echo "Holberton School for the win!" | sudo tee /var/www/html/index.nginx-debian.html && var="rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" && sudo sed -i "35i$var" /etc/nginx/sites-available/default && sudo echo "Ceci n\'est pas une page" | sudo tee /var/www/html/custom_404.html && var2="error_page 404 /custom_404.html;\nlocation /custom_404.html {\nroot /var/www/html;\ninternal;\n}" && sudo sed -i "36i$var2" /etc/nginx/sites-available/default && var4="add_header X-Served-By $HOSTNAME;" && sudo sed -i "41i$var4" /etc/nginx/sites-available/default && sudo service nginx start',
   provider => shell
 }
