a40ed4d9093675e46c1b2732adfd1b6066daadc5
jenntang1 <1039@holbertonschool.com>
2020-05-04 21:54:44 +0000

    3. Adding fieldnames to identify key in dictionaries

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -24,19 +24,18 @@ if __name__ == "__main__":
             format(empl_id)
         response = requests.get(tasks_url)
         todos = response.json()
-        completed = response.json()
 
         """ Get NUMBER_OF_DONE_TASKS """
         tasks_done = 0
         tasks_list = []
-        for key in completed:
+        for key in todos:
             if key["completed"] is True:
                 tasks_done += 1
                 tasks_list.append(key["title"])
 
         """ Get TOTAL_NUMBER_OF_TASKS """
         tasks_total = 0
-        for key in completed:
+        for key in todos:
             if key["title"]:
                 tasks_total += 1
 
@@ -50,13 +49,15 @@ if __name__ == "__main__":
                 print("\t {}".format(element))
 
         else:
+            """ Exports to CSV """
             with open("{}.csv".format(empl_id), "w") as csv_file:
-                tasks_writer = csv.writer(csv_file,
-                                          delimiter=",",
-                                          quotechar='"',
-                                          quoting=csv.QUOTE_ALL)
-                todos_list = []
+                columns = ["userId", "id", "completed", "title"]
+                tasks_writer = csv.DictWriter(csv_file,
+                                              delimiter=",",
+                                              quotechar='"',
+                                              quoting=csv.QUOTE_ALL,
+                                              fieldnames=columns)
+                todos_dict = {}
                 for dicts in todos:
-                    for key, value in dicts.items():
-                        todos_list.append(value)
-                tasks_writer.writerow(todos_list)
+                    todos_dict.update(dicts)
+                    tasks_writer.writerow(todos_dict)
