ad1ae923ab60dfeb2047e8c8d6454242efb4d881
jenntang1 <1039@holbertonschool.com>
2020-05-04 23:15:46 +0000

    4. Changing id field to username field

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,23 +9,23 @@ import sys
 
 if __name__ == "__main__":
     """ If second argument is an integer, then it's an employee id """
-    run_script = sys.argv[0]
     empl_id = sys.argv[1]
     if empl_id.isdigit():
 
-        """ Get EMPLOYEE_NAME """
+        """ Gets EMPLOYEE_NAME """
         empl_url = "https://jsonplaceholder.typicode.com/users/{}".\
             format(empl_id)
         response = requests.get(empl_url)
         empl_name = response.json().get("name")
+        username = response.json().get("username")
 
-        """ Get tasks assigned to employee """
+        """ Gets tasks assigned to employee """
         tasks_url = "https://jsonplaceholder.typicode.com/users/{}/todos".\
             format(empl_id)
         response = requests.get(tasks_url)
         todos = response.json()
 
-        """ Get NUMBER_OF_DONE_TASKS """
+        """ Gets NUMBER_OF_DONE_TASKS """
         tasks_done = 0
         tasks_list = []
         for key in todos:
@@ -33,31 +33,35 @@ if __name__ == "__main__":
                 tasks_done += 1
                 tasks_list.append(key["title"])
 
-        """ Get TOTAL_NUMBER_OF_TASKS """
+        """ Gets TOTAL_NUMBER_OF_TASKS """
         tasks_total = 0
         for key in todos:
             if key["title"]:
                 tasks_total += 1
+        print("Employee {} is done with tasks({}/{}):".format(empl_name,
+                                                              tasks_done,
+                                                              tasks_total))
 
-        """ Print data to standard out """
-        if run_script == "0-gather_data_from_an_API.py":
-            print("Employee {} is done with tasks({}/{}):".format(empl_name,
-                                                                  tasks_done,
-                                                                  tasks_total))
+        """ Gets TASK_TITLE """
+        for element in tasks_list:
+            print("\t {}".format(element))
 
-            for element in tasks_list:
-                print("\t {}".format(element))
+        """ Adds EMPLOYEE_NAME to the todos data """
+        todos_list = list(todos)
+        name_pair = {"username": username}
+        for dicts in todos_list:
+            dicts.update(name_pair)
 
-        else:
-            """ Exports to CSV """
-            with open("{}.csv".format(empl_id), "w") as csv_file:
-                columns = ["userId", "id", "completed", "title"]
-                tasks_writer = csv.DictWriter(csv_file,
-                                              delimiter=",",
-                                              quotechar='"',
-                                              quoting=csv.QUOTE_ALL,
-                                              fieldnames=columns)
-                todos_dict = {}
-                for dicts in todos:
-                    todos_dict.update(dicts)
-                    tasks_writer.writerow(todos_dict)
+        """ Exports to CSV """
+        with open("{}.csv".format(empl_id), "w") as csv_file:
+            columns = ["userId", "username", "completed", "title"]
+            tasks_writer = csv.DictWriter(csv_file,
+                                          delimiter=",",
+                                          quotechar='"',
+                                          quoting=csv.QUOTE_ALL,
+                                          fieldnames=columns,
+                                          extrasaction="ignore")
+            todos_dict = {}
+            for dicts in todos_list:
+                todos_dict.update(dicts)
+                tasks_writer.writerow(todos_dict)
