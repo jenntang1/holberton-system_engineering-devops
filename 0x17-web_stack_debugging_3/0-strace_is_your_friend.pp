80afd2ed71203f151795fd26604f1f08bba78164
jenntang1 <1039@holbertonschool.com>
2020-05-20 13:07:01 +0000

    Fix style compliance

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # This manifest resolves a Wordpress 500 error.
 
 exec { 'fix-wordpress':
-	command  => 'sudo sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php && sudo service apache2 restart',
-	provider => shell
+  command  => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  provider => shell
 }
