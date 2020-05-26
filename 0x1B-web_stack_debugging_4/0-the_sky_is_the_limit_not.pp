f6f65ae4efe7d372e1565dacd5c7b86d9bc956a1
jenntang1 <1039@holbertonschool.com>
2020-05-26 17:44:44 +0000

    2. Updating with more details

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # This manifest resolves heavy HTTP requests.  
 
 exec { 'fix--for-nginx':
-  command  => 'sudo echo "nginx\t soft\t nofile\t 30000\nginx\t hard\t nofile\t 50000\n"'\
-              '| sudo tee -a /etc/security/limits.conf && sudo service nginx restart',
+  command  => 'sudo echo "nginx\t soft\t nofile\t 30000\nnginx\t hard\t nofile\t 50000\n"'\
+              ' | sudo tee -a /etc/security/limits.conf && sudo service nginx restart',
   provider => shell
 }
--- 0x1B-web_stack_debugging_4/README.md
+++ 0x1B-web_stack_debugging_4/README.md
@@ -8,5 +8,8 @@ https://www.tutorialspoint.com/apache_bench/index.htm
 1. How to use ApacheBench for web server performance testing  
 https://www.datadoghq.com/blog/apachebench/  
 
+2. Nginx socket() failed (24: Too many open files)  
+https://www.claudiokuenzler.com/blog/850/nginx-socket-failed-24-too-many-open-files  
+
 ## Contributor ##
 Jennifer Tang  
