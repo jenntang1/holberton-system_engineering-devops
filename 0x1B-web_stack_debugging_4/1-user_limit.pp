dd5ec859df27f5b99b8a6021fff4bd59d9a06fd8
jenntang1 <1039@holbertonschool.com>
2020-05-26 18:16:07 +0000

    1. Creating puppet manifest to debug too many file opened error

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,7 @@
+# This manifest resolves heavy HTTP requests.  
+
+exec { 'change-os-configuration-for-holberton-user':
+  command  => 'sudo sed -i "s/holberton hard nofile 5/holberton hard nofile 50000/g" /etc/security/limits.conf'\
+              ' && sudo sed -i "s/holberton soft nofile 4/holberton soft nofile 40000/g" /etc/security/limits.conf',
+  provider => shell
+}
