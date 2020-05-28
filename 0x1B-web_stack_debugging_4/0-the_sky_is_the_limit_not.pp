0e0ff3b40c7be674dc0ac51c5212af8d14b23c4e
jenntang1 <1039@holbertonschool.com>
2020-05-28 15:23:08 +0000

    4. Updating to add worker_rlimit_nofile

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,8 @@
 # This manifest resolves heavy HTTP requests.
 
 exec { 'fix--for-nginx':
-  command  => 'sudo sed -i "s/15/5000/g" /etc/default/nginx',
+  command  => 'sudo sed -i "4iworker_rlimit_nofile 500000\n" /etc/nginx/nginx.conf && '\
+              'sudo echo "nginx\tsoft\tnofile\t500000\nnginx\thard\tnofile\t500000\n"'\
+              ' | sudo tee -a /etc/security/limits.conf && sudo service nginx restart',
   provider => shell
 }
