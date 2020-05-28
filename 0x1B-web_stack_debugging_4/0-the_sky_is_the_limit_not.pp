2fbffc6b65df850d024f36b5dfd63d5ba089ad67
jenntang1 <1039@holbertonschool.com>
2020-05-28 15:51:15 +0000

    5. Updating to set limit system-wide

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,7 @@
 # This manifest resolves heavy HTTP requests.
 
 exec { 'fix--for-nginx':
-  command  => 'sudo sed -i "4iworker_rlimit_nofile 500000\n" /etc/nginx/nginx.conf && '\
-              'sudo echo "nginx\tsoft\tnofile\t500000\nnginx\thard\tnofile\t500000\n"'\
-              ' | sudo tee -a /etc/security/limits.conf && sudo service nginx restart',
+  command  => 'sudo sed -i "12iLimitNOFILE" /lib/systemd/system/nginx.service '\
+              '&& sudo service nginx restart',
   provider => shell
 }
