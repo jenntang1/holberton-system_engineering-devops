5a7d728dfecb6affb4c61e279debc24dad8b2421
jenntang1 <1039@holbertonschool.com>
2020-05-28 17:50:35 +0000

    7. Adding nginx restart command

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # This manifest resolves heavy HTTP requests.
 
 exec { 'fix--for-nginx':
-  command  => 'sudo sed -i "s/-n */-n 5000000/g" /etc/default/nginx',
+  command  => 'sudo sed -i "s/-n */-n 5000000/g" /etc/default/nginx && sudo service nginx restart',
   provider => shell
 }
