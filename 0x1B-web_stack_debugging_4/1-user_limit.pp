657ff4eeb68b34398e1aa9b3f6d1f0b3b6b5db21
jenntang1 <1039@holbertonschool.com>
2020-05-28 18:44:47 +0000

    3. Simplifying sed command

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,8 +1,7 @@
-# This manifest resolves heavy HTTP requests.  
+# This manifest resolves heavy HTTP requests.
 
 exec { 'change-os-configuration-for-holberton-user':
-  command  => 'sudo sed -i "s/holberton hard nofile 5/holberton hard nofile 50000/g" /etc/security/limits.conf'\
-              ' && sudo sed -i "s/holberton soft nofile 4/holberton soft nofile 50000/g" /etc/security/limits.conf'\
-              ' && sudo service nginx restart',
+  command  => 'sudo sed -i -e "s/holberton hard nofile 5/holberton hard nofile 50000/g"'\
+              '-e "s/holberton soft nofile 4/holberton soft nofile 50000/g" /etc/security/limits.conf',
   provider => shell
 }
