0637065cd1841e349d32801d92b4d57d8650f9f0
jenntang1 <1039@holbertonschool.com>
2020-05-28 18:23:24 +0000

    2. Adding nginx restart

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,6 +2,7 @@
 
 exec { 'change-os-configuration-for-holberton-user':
   command  => 'sudo sed -i "s/holberton hard nofile 5/holberton hard nofile 50000/g" /etc/security/limits.conf'\
-              ' && sudo sed -i "s/holberton soft nofile 4/holberton soft nofile 40000/g" /etc/security/limits.conf',
+              ' && sudo sed -i "s/holberton soft nofile 4/holberton soft nofile 50000/g" /etc/security/limits.conf'\
+              ' && sudo service nginx restart',
   provider => shell
 }
