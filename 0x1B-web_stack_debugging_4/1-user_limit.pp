f77cad12cf586c3b020b97c2474f84986526f505
jenntang1 <1039@holbertonschool.com>
2020-05-28 19:02:17 +0000

    6. Updating to use limits module

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,8 @@
 # This manifest resolves heavy HTTP requests.
 
-exec { 'change-os-configuration-for-holberton-user':
-  command  => 'sudo sed -i -e "s/holberton hard nofile 5/holberton hard nofile 50000/g"'\
-              ' -e "s/holberton soft nofile 4/holberton soft nofile 50000/g" /etc/security/limits.conf',
-  provider => shell
+include limits
+limits::limit { 'user limit':
+  domain => 'holberton',
+  item   => 'nofile',
+  value  => '50000'
 }
