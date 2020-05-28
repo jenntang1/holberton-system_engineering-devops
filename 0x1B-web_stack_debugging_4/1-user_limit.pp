beef818570bfce41e3fd96d9ca3c84df5f751dfb
jenntang1 <1039@holbertonschool.com>
2020-05-28 18:49:28 +0000

    5. Updating to be in compliance with puppet-lint

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,7 @@
 # This manifest resolves heavy HTTP requests.
 
 exec { 'change-os-configuration-for-holberton-user':
-  command  => 'sudo sed -i -e "s/holberton hard nofile 5/holberton hard nofile 50000/g" -e "s/holberton soft nofile 4/holberton soft nofile 50000/g" /etc/security/limits.conf',
+  command  => 'sudo sed -i -e "s/holberton hard nofile 5/holberton hard nofile 50000/g"'\
+              ' -e "s/holberton soft nofile 4/holberton soft nofile 50000/g" /etc/security/limits.conf',
   provider => shell
 }
