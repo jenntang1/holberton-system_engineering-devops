79a7ee35326eb734bfbeb89b8b4b189db2cb2632
jenntang1 <1039@holbertonschool.com>
2020-05-28 14:03:28 +0000

    3. Updating with correct bash command

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,6 @@
-# This manifest resolves heavy HTTP requests.  
+# This manifest resolves heavy HTTP requests.
 
 exec { 'fix--for-nginx':
-  command  => 'sudo echo "nginx\t soft\t nofile\t 30000\nnginx\t hard\t nofile\t 50000\n"'\
-              ' | sudo tee -a /etc/security/limits.conf && sudo service nginx restart',
+  command  => 'sudo sed -i "s/15/5000/g" /etc/default/nginx',
   provider => shell
 }
